eb10cefb7d05e5dec712a6ee76477312917a614f
Demonscript <demonscript.io@gmail.com>
2020-06-12 00:16:21 -0500

    ver 0.83
    Creación de la App services e implementación inicial de su vista y su template services.html configuración de dicho cambio en urls.py. Proyecto funcionando y al igual que la anterior versión, sin mostrar los proyectos de la BD en services.html

--- Django_project1/urls.py
+++ Django_project1/urls.py
@@ -15,15 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core import views as core_views
-from services import views as services_views
+from core import views
 from django.conf import settings
 
 urlpatterns = [
-    path('contact/',core_views.contact,name = 'contacto'),
-    path('services',services_views.services,name = 'servicios'),
-    path('',core_views.index,name = 'inicio'),
-    path('info/',core_views.info,name = 'informacion'),
+    path('contact/',views.contact,name = 'contacto'),
+    path('services',views.services,name = 'servicios'),
+    path('',views.index,name = 'inicio'),
+    path('info/',views.info,name = 'informacion'),
     path('admin/', admin.site.urls),
 ]
 
--- services/views.py
+++ services/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-def services(request):
-    return render(request, "services/services.html")
