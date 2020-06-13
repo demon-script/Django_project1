9d9bc2ed20960d86971084f06a46a1402f82e957
Demonscript <demonscript.io@gmail.com>
2020-06-13 14:36:26 -0500

    Ver 1.0 stable
    Proyecto Django con su primera versión estable. Con conexión a la BD para mostrar los proyectos correctamente  en service.html

--- services/models.py
+++ services/models.py
@@ -6,7 +6,9 @@ from pip._vendor.chardet.cli.chardetect import description_of
 class New_Project(models.Model):
     title = models.CharField(max_length = 80, verbose_name = 'Nombre del Proyecto')
     description = models.TextField(verbose_name = 'Descripción')
+    more_info = models.URLField(null = True, blank = True,verbose_name='Sitio Web')
     image = models.ImageField(upload_to = 'projects',verbose_name = 'Imagen')
+    
     created = models.DateTimeField(auto_now_add = True, verbose_name = 'F. Creación')
     modified = models.DateTimeField(auto_now = True, verbose_name = 'F. Modificación')
     
--- services/views.py
+++ services/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from services.models import New_Project
 
 # Create your views here.
 def services(request):
-    return render(request, "services/services.html")
+    projects = New_Project.objects.all()
+    return render(request, "services/services.html",{'projects':projects})
